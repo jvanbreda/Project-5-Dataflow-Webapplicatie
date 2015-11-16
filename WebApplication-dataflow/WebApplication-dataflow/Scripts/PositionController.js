7970b9598648a3af9d7e9310154c42f2697b783d
kevin2106 <keviee007@live.nl>
2015-11-16 23:54:39 +0100

    0.1

---
+++ WebApplication-dataflow/WebApplication-dataflow/Scripts/PositionController.js
@@ -0,0 +1,11 @@
+﻿var PositionInfoController = angular.module('PositionsApp', []);
+
+PositionInfoController.controller('PositionController', function ($scope, $http) {
+    $http.get("http://145.24.222.160/DataFlowWebservice/api/positions/357566000058106")
+    .then(function (response) {
+        $scope.names = response.data.result;
+    })
+    .catch(function (response) {
+        alert("HTTP Request failed: " + response.data.status);
+    })
+})
