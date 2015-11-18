a4c39bdd4d1793f0287cf6edebfb4c6603243dbf
Lars van Hilten <larsvh8@gmail.com>
2015-11-18 17:54:46 +0100

    Initial push

---
+++ WebApplication-dataflow/WebApplication-dataflow/Scripts/buydata.js
@@ -0,0 +1,26 @@
+﻿angular.module('BuyData', [])
+ .controller('SelectController', ['$scope', function ($scope) {
+
+     $scope.dataSelect = null;
+
+     $scope.dateFrom = null;
+     $scope.dateTo = null;
+
+     $scope.stepCount = 0;
+
+     $scope.update = function () {
+
+         switch ($scope.stepCount) {
+             case 0:
+                 var guideSteps = angular.element(document.querySelector('#guideSteps'));
+                 guideSteps.append("<p>2. Select the time range of your data.</p>");
+                 break;
+
+             case 1:
+                 alert($scope.dateFrom);
+         }
+        $scope.stepCount++;
+     };
+
+
+ }]);
