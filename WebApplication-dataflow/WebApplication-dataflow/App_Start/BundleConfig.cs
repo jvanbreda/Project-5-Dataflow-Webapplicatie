ff91297e32059e3054529e7d0b4d45121295c649
swenmeeuwes <swenmeeu@gmail.com>
2015-11-20 19:06:08 +0100

    ADDED: Dashboard css

--- WebApplication-dataflow/WebApplication-dataflow/App_Start/BundleConfig.cs
+++ WebApplication-dataflow/WebApplication-dataflow/App_Start/BundleConfig.cs
@@ -22,6 +22,10 @@ namespace WebApplication_dataflow {
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new StyleBundle("~/Content/DashboardStyle").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/Dashboard.css"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ WebApplication-dataflow/WebApplication-dataflow/Scripts/AngularControllers/DashboardController.js
@@ -0,0 +1,5 @@
+﻿var app = angular.module('datflowApp', []);
+app.controller('dashboardController', function ($scope, $http) {
+    $http.get("http://www.w3schools.com/angular/customers.php")
+    .success(function (response) { $scope.names = response.records; });
+});
