9ebc7fed4fa8cbb96f6ceda51a2756034619f741
Lars van Hilten <larsvh8@gmail.com>
2015-11-23 18:58:25 +0100

    Second push

--- WebApplication-dataflow/WebApplication-dataflow/Scripts/buydata.js
+++ WebApplication-dataflow/WebApplication-dataflow/Scripts/buydata.js
@@ -1,26 +1,65 @@
-﻿angular.module('BuyData', [])
- .controller('SelectController', ['$scope', function ($scope) {
+﻿var app = angular.module('BuyData', []);
 
+ app.controller('SelectController', function ($scope, $http) {
      $scope.dataSelect = null;
 
-     $scope.dateFrom = null;
-     $scope.dateTo = null;
+     //current day
+     $scope.dateFrom = new Date();
+     //current day + 1 day
+     $scope.dateTo = new Date($scope.dateFrom.getTime() + 86400000);
 
      $scope.stepCount = 0;
 
      $scope.update = function () {
+         var guideSteps = angular.element(document.querySelector('#guideSteps'));
 
          switch ($scope.stepCount) {
              case 0:
-                 var guideSteps = angular.element(document.querySelector('#guideSteps'));
-                 guideSteps.append("<p>2. Select the time range of your data.</p>");
+                 guideSteps.append('<p id="0">2. Select the data type of the file.</p>');
+                 break;
+             case 1:
+                 guideSteps.append('<p id="1">3. Select the start date of your data.</p>');
+                 break;
+             case 2:
+                 guideSteps.append('<p id="2">4. Select the end date of your data.</p>');
+                 break;
+             case 3:
+                 if ($scope.dateTo < $scope.dateFrom) {
+                     alert("The end date must be larger or equal to the start date!");
+                     return;
+                 }
                  break;
 
-             case 1:
-                 alert($scope.dateFrom);
          }
-        $scope.stepCount++;
+         $scope.stepCount++;
+         $scope.fillExample();
      };
 
+     $scope.previous = function () {
+         if($scope.stepCount > 0){
+             $scope.stepCount--;
+             var paragraph = document.getElementById($scope.stepCount);
+             paragraph.remove();
+             //TODO: check on last step
+         }
+         
+     };
 
- }]);
+     $scope.buy = function () {
+        
+     };
+
+     $scope.fillExample = function () {
+         $http.get("http://145.24.222.160/DataFlowWebservice/api/" + $scope.dataSelect)
+         .then(function (response) {
+             //$scope.code = response.data.result;
+             $scope.code = angular.fromJson(response.data);
+         })
+         .catch(function (response) {
+             alert("HTTP Request failed: " + response.data.status);
+         })
+     }
+
+ }
+
+ );
