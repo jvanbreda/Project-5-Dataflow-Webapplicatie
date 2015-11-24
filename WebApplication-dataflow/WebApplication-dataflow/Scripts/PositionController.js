89e571a7d01abce35f8348b87f357177617f378d
kevin2106 <keviee007@live.nl>
2015-11-24 01:09:30 +0100

    0.2

--- WebApplication-dataflow/WebApplication-dataflow/Scripts/PositionController.js
+++ WebApplication-dataflow/WebApplication-dataflow/Scripts/PositionController.js
@@ -1,11 +1,58 @@
 ﻿var PositionInfoController = angular.module('PositionsApp', []);
 
-PositionInfoController.controller('PositionController', function ($scope, $http) {
-    $http.get("http://145.24.222.160/DataFlowWebservice/api/positions/357566000058106")
-    .then(function (response) {
-        $scope.names = response.data.result;
-    })
-    .catch(function (response) {
-        alert("HTTP Request failed: " + response.data.status);
-    })
-})
+PositionInfoController.service('HTTPController', function ($http) {
+    var getData = function () {
+        return $http.get("http://145.24.222.160/DataFlowWebservice/api/events")
+         .then(function (response) {
+             return response.data.result;
+         })
+         .catch(function (response) {
+             alert("HTTP Request failed: " + response.status);
+         })
+    };
+    return { getData: getData };
+});
+
+PositionInfoController.controller("DataAnalysisController", ['$scope', 'HTTPController', function ($scope, HTTPController) {
+    var data = HTTPController.getData();
+    data.then(function (response) {
+        $scope.units = response;
+        var unitarray = [];
+        var nodupl = [];
+        var unitcount = {};
+        var total = nodupl.concat(unitcount);
+        
+
+        
+
+        for (var i = 0; i < $scope.units.length; i++) {
+            unitarray.push($scope.units[i].unitId);
+        }
+
+        for (var i = 0; i < unitarray.length; i++) {
+            if (!unitcount[unitarray[i]])
+                unitcount[unitarray[i]] = 0;
+            ++unitcount[unitarray[i]];
+        }
+
+        nodupl = eliminateDuplicates(unitarray);
+        $scope.ids = total;
+
+
+
+        function eliminateDuplicates(array) {
+            var i,
+                length = array.length,
+                out = [],
+                obj = {};
+            for (i = 0; i < length; i++) {
+                obj[array[i]] = 0;
+            }
+            for (i in obj) {
+                out.push(i);
+            }
+            return out;
+        }
+    }) 
+}
+])
