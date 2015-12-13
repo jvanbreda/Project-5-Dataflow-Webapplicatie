1fb4997cbce27a1d55f54c254799ea41c0df52f8
Lars van Hilten <larsvh8@gmail.com>
2015-12-13 20:56:24 +0100

    Example code

---
+++ WebApplication-dataflow/TestApplication-dataflow/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestApplication_dataflow
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShouldReturnTrue()
+        {
+            bool result = true;
+            Assert.IsTrue(result);
+        }
+    }
+}
