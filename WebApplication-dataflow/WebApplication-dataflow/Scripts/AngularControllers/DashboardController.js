29e59b0509453e835c317e6a9fd1427a3a31eb3e
swenmeeuwes <swenmeeu@gmail.com>
2016-01-04 19:23:02 +0100

    Added: Maintenance analysis

--- WebApplication-dataflow/WebApplication-dataflow/Scripts/AngularControllers/DashboardController.js
+++ WebApplication-dataflow/WebApplication-dataflow/Scripts/AngularControllers/DashboardController.js
@@ -1,5 +1,8 @@
 ﻿var app = angular.module('dataflowApp', []);
 
-app.controller('dashboardController', function ($scope) {
-    
+app.controller('dashboardController', function ($scope, $http) {
+    $scope.readyMaintenance = false;
+
+    // Should go in config file
+    $http.get("http://145.24.222.160/DataflowAnalyseservice/api/maintenance").then(function (response) { $scope.maintenance = response.data.result; }).then(function () { $scope.readyMaintenance = true; });
 });
