7a451094bf8e51eedf142fbd913d7edb371c8ace
gvh1993 <gertjanvanh@live.nl>
2016-01-07 14:13:20 +0100

    got dummy pie chart and the rawdata from webapi displayed

--- WebApplication-dataflow/WebApplication-dataflow/Scripts/AngularControllers/DiskSpaceAnalysis/DiskSpaceController.js
+++ WebApplication-dataflow/WebApplication-dataflow/Scripts/AngularControllers/DiskSpaceAnalysis/DiskSpaceController.js
@@ -1,4 +1,4 @@
-﻿var diskSpaceApp = angular.module("diskSpaceApp", []);
+﻿var diskSpaceApp = angular.module("diskSpaceApp", ['nvd3']);
 
 diskSpaceApp.service('HTTPService', function ($http) {
     var getData = function () {
@@ -18,5 +18,59 @@ diskSpaceApp.controller("diskSpaceController", ['$scope','HTTPService',function 
     var dataPromise = HTTPService.getData();
     dataPromise.then(function (response) {
         $scope.init = response;
+
+        $scope.options = {
+            chart: {
+                type: 'pieChart',
+                height: 500,
+                x: function (d) { return d.key; },
+                y: function (d) { return d.y; },
+                showLabels: true,
+                duration: 500,
+                labelThreshold: 0.01,
+                labelSunbeamLayout: true,
+                legend: {
+                    margin: {
+                        top: 5,
+                        right: 35,
+                        bottom: 5,
+                        left: 0
+                    }
+                }
+            }
+        };
+
+        $scope.data = [
+            {
+                key: "One",
+                y: 5
+            },
+            {
+                key: "Two",
+                y: 2
+            },
+            {
+                key: "Three",
+                y: 9
+            },
+            {
+                key: "Four",
+                y: 7
+            },
+            {
+                key: "Five",
+                y: 4
+            },
+            {
+                key: "Six",
+                y: 3
+            },
+            {
+                key: "Seven",
+                y: .5
+            }
+        ];
     })
 }]);
+
+
